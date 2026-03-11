79efcc1a8993fd7faa4e63699cefe114764a0a8a
Li <113808480+Li2548@users.noreply.github.com>
2026-03-12 07:29:25 +0800

    更新 AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,3 +1,4 @@
+import fl_pip
 import Flutter
 import UIKit
 
@@ -8,10 +9,14 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     application.applicationSupportsShakeToEdit = false // Disable shake to undo
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
+
+    override func registerPlugin(_ registry: FlutterPluginRegistry) {
+        GeneratedPluginRegistrant.register(with: registry)
 }
